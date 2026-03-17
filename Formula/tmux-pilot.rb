8e78120837530d97701ff394e77cf5dcbcf0cd23
Alexandro T. Netto <56097505+alextnetto@users.noreply.github.com>
2026-03-17 04:26:06 +0000

    fix: update tmux-pilot to v0.4.0

--- Formula/tmux-pilot.rb
+++ Formula/tmux-pilot.rb
@@ -1,28 +1,28 @@
 class TmuxPilot < Formula
   desc "A TUI for managing tmux sessions"
   homepage "https://github.com/blockful/tmux-pilot"
-  version "1.0.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Darwin_arm64.tar.gz"
-      sha256 "152e23ede7c3e69cbc75398729cff542fbc915730055a5ea8f849ffaa4b7d171"
+      url "https://github.com/blockful/tmux-pilot/releases/download/v0.4.0/tmux-pilot_Darwin_arm64.tar.gz"
+      sha256 "0739f43c7cbbe4fe25050d8133c7f469a7493bd78de2d06d1612ab1d587c230a"
     end
     on_intel do
-      url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Darwin_x86_64.tar.gz"
-      sha256 "bf7f83e565550fe3c0542e2dc25d8c576187e0e738f164d95730338a7fa1f6eb"
+      url "https://github.com/blockful/tmux-pilot/releases/download/v0.4.0/tmux-pilot_Darwin_x86_64.tar.gz"
+      sha256 "cc9955330f0ebc9578b0ca7e9423debba9e2f294fb4ff788c7622dd3ce4a08a7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Linux_arm64.tar.gz"
-      sha256 "3dbbd6edc3bf81c5c7a4ae7fefe5f9acec5efce230c6c304818d97442240e946"
+      url "https://github.com/blockful/tmux-pilot/releases/download/v0.4.0/tmux-pilot_Linux_arm64.tar.gz"
+      sha256 "b0875d0469f3b2c8b6a8b581770000ee393cab0e464df4f0a3f84c0d18b79a13"
     end
     on_intel do
-      url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Linux_x86_64.tar.gz"
-      sha256 "42a92d9b24974e6eae910cbf3736c0301a5c0c55236c8f75b3d5ebb6892d7360"
+      url "https://github.com/blockful/tmux-pilot/releases/download/v0.4.0/tmux-pilot_Linux_x86_64.tar.gz"
+      sha256 "d1c9ad4d9cfb64bf17f73f0e2f2379f124e003496be5424f418bc8c175a39db7"
     end
   end
 
