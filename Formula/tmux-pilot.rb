71a358afb3ca863412fb3dc210527fee1e4e07e9
Alexandro T. Netto <56097505+alextnetto@users.noreply.github.com>
2026-03-18 03:31:18 +0000

    v1.0.0 — raw ANSI rewrite, final checksums

--- Formula/tmux-pilot.rb
+++ Formula/tmux-pilot.rb
@@ -7,22 +7,22 @@ class TmuxPilot < Formula
   on_macos do
     on_arm do
       url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Darwin_arm64.tar.gz"
-      sha256 "2be04206e5715498f48fe5fb39e938e6867bf26ab519586d9b5bd35fd5f9eacb"
+      sha256 "2042eb7ad1fd3730d870d9094df2b73b8827a699fc4b9b14cdc3bfe2836a0feb"
     end
     on_intel do
       url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Darwin_x86_64.tar.gz"
-      sha256 "0216d7098936bfaaf840889ede477b869144e6e9765e94d318be1116efc5b9ae"
+      sha256 "47a96c2d41539a078980981568aa10a0d83812430850176c2e18cf197bdb6c5b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Linux_arm64.tar.gz"
-      sha256 "4338b10afd7172883e519286a89ba9ceb991c4fab5af8820944bdddec942e6d8"
+      sha256 "570f9fa58e7e5265dc232f5bb99a6d300ed75b448e93da9a81934167b06a930d"
     end
     on_intel do
       url "https://github.com/blockful/tmux-pilot/releases/download/v1.0.0/tmux-pilot_Linux_x86_64.tar.gz"
-      sha256 "d11a3820facee5c893113623a5a684e0f3632effb4e341ec32535ca6f8a2c163"
+      sha256 "a393eb71b8043dc7a8422ae7c9be829dd711a010f3af9202b93f0add102f54b3"
     end
   end
 
